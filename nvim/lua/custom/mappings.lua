e13e111266df3ffedfe3be612aa7b1874a90b133
Nicola Benaglia <nico.benaz@gmail.com>
2023-11-05 01:08:18 +0100

    Tmux config

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -1,5 +1,14 @@
 local M = {}
 
+M.general = {
+  n = {
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "windows left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "windows right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "windows down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "windows up" },
+  }
+}
+
 M.dap = {
   plugin = true,
   n = {
